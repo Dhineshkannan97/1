49d3829a5deeac5f985ddf9721d02ef236d466e8
dhinesh <dhineshkannan.madhavan@gcitsolutions.com>
2022-10-03 14:13:27 +0530

    1

---
+++ src/main/java/Demo.java
@@ -0,0 +1,38 @@
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+    int a []={888};
+    int b[] = {777};
+
+
+     List <Integer> d= new ArrayList<Integer>();
+
+
+
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <a.length; i++) {
+             d.add(a[i]);
+            for (int j = 0; j < b.length; j++) {
+                d.add(b[j]);
+            }
+        }
+
+         system.out.println(d);
+
+
+    }
+
+
+
+
+
+
+}
